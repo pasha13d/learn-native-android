c480c5dff71440a30dea31ea43738315f9faeb4e
mahabub.pasha <mahbub.pasha@randomaccessinc.com>
2022-11-04 10:47:02 +0600

    learing edit text, button and toaster

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.testapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+            val greetingTextView = findViewById<TextView>(R.id.tvHello)
+            val inputField = findViewById<EditText>(R.id.txtName)
+            val submitBtn = findViewById<Button>(R.id.btnSubmit)
+            submitBtn.setOnClickListener {
+                val enteredName = inputField.text.toString()
+                if(enteredName == "") {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Please enter your name!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    val message = "Welcome $enteredName"
+                    greetingTextView.text = message
+                    inputField.text.clear()
+                }
+            }
     }
 }
